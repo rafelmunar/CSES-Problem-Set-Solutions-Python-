decec5a2eab76e6f24eeef50ebbb52a4fa0aeb0a
rafelmunar <rafelmunarmarroig@icloud.com>
2026-01-30 18:03:55 +0100

    Delete CSES Problem Set directory

--- CSES Problem Set/25. CSES Problem Set_Distinct Numbers.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-arr = list(map(int, input().split()))
-
-distinct_set = set(arr)
-print(len(distinct_set))
--- CSES Problem Set/9. CSES Problem Set_Bit Strings.py
+++
@@ -1,7 +0,0 @@
-MOD = 10**9 + 7
-
-n = int(input())
-
-result = pow(2, n, MOD)
-
-print(result)
